395a7b13064386a1017d599c5d3ab70a0acae691
gerarr <gerarr999@yandex.ru>
2026-01-06 14:15:57 +0300

    add invalidPasswordTest

--- src/main/java/org/example/ui/page/MainPage.java
+++ src/main/java/org/example/ui/page/MainPage.java
@@ -3,6 +3,7 @@ package org.example.ui.page;
 import com.codeborne.selenide.SelenideElement;
 import io.qameta.allure.Step;
 
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$x;
 
 public class MainPage {
@@ -10,24 +11,32 @@ public class MainPage {
     private SelenideElement loginField = $x("//input[@data-test='username']");
     private SelenideElement passwordField = $x("//input[@data-test='password']");
     private SelenideElement loginButton = $x("//input[@data-test='login-button']");
+    private SelenideElement errorText = $x("//h3");
 
     public static MainPage of() {
         return new MainPage();
     }
+
     @Step("Нажал на кнопку логина")
     public void clickLoginButton() {
         loginButton.click();
     }
+
     @Step("Вписал логин")
     public MainPage inputLogin(String login) {
         loginField.setValue(login);
         return this;
 
     }
+
     @Step("Вписал пороль")
     public MainPage inputPassword(String password) {
         passwordField.setValue(password);
         return this;
+    }
+
+    public String getErrorText() {
+        return errorText.getText();
     }
 
 }
--- src/test/java/test/test.java
+++ src/test/java/test/test.java
@@ -4,6 +4,7 @@ import com.codeborne.selenide.Selenide;
 import extensions.SelenideExtensions;
 import org.example.ui.page.MainPage;
 import org.example.utils.config.ConfigData;
+import org.example.utils.text.InvalidAuthorization;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -23,5 +24,17 @@ public class test {
         String currentUrl = Selenide.webdriver().object().getCurrentUrl();
 
         assertThat(currentUrl).isEqualTo(ConfigData.INVENTORY.getValue());
+    }
+
+    @Test
+    @DisplayName("Авторизация с неправильным паролем")
+    public void invalidPasswordTest() {
+        MainPage.of().inputLogin(ConfigData.LOGIN.getValue())
+                .inputPassword("1")
+                .clickLoginButton();
+
+        String text = MainPage.of().getErrorText();
+
+        assertThat(text).isEqualTo(InvalidAuthorization.INVALID_PASSWORD);
     }
 }
